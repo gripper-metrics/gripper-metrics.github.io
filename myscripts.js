9a0e9d52b983aa3b05cdd261be2b11f6387b2a22
gripper-metrics <36992716+gripper-metrics@users.noreply.github.com>
2018-03-02 16:46:21 +0100

    Create myscripts.js

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function show(shown, hidden) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  return false;
+}
+
+function metricstime() {
+  var OS = document.getElementById(OS);
+  var OW = document.getElementById(OW);
+  var OH = document.getElementById(OH);
+  var OM = document.getElementById(OM);
+  var RA = document.getElementById(RA);
+  var RP = document.getElementById(RP);
+  var Task = document.getElementById(Task);
+  
