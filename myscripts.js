b51b71644b0bd5fd60a9f06c72cf289159241f4b
gripper-metrics <36992716+gripper-metrics@users.noreply.github.com>
2018-03-05 16:29:04 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -13,7 +13,7 @@ function metricstime() {
   var RoAc = document.getElementById('RA');
   var RoPl = document.getElementById('RP');
   var Task = document.getElementById('Task');
-  
+    
   //alert(ObSi.value ObWe.value ObSh.value ObMa.value RoAc.value RoPl.value Task.value);
 }
  
