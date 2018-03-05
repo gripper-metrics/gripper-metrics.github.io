29775aee151c63a53ab44ec2bba48d5cb04a8686
gripper-metrics <36992716+gripper-metrics@users.noreply.github.com>
2018-03-05 16:28:34 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -2,7 +2,7 @@ function show(shown, hidden) {
   document.getElementById(shown).style.display='block';
   document.getElementById(hidden).style.display='none';
   return false;
-}
+}  
     
 function metricstime() {
   alert('hola')
